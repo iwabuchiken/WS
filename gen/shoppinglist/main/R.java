015b9f2ec5b86d31a66686bdc3cef32d5c979882
iwabuchiken <iwabuchi.k.2010@gmail.com>
2012-07-02 22:36:55 +0900

    B1
    v.1
    d.1
    
    D-------------------------------
    1. Up to => LL: item

--- gen/shoppinglist/main/R.java
+++ gen/shoppinglist/main/R.java
@@ -14,11 +14,18 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int v1_=0x7f060001;
-        public static final int v1_menu_1=0x7f060000;
+        public static final int EditText01=0x7f060005;
+        public static final int v1_=0x7f060007;
+        public static final int v1_LL_item=0x7f060003;
+        public static final int v1_LL_store=0x7f060000;
+        public static final int v1_et_store=0x7f060002;
+        public static final int v1_menu_1=0x7f060006;
+        public static final int v1_tv_item=0x7f060004;
+        public static final int v1_tv_store=0x7f060001;
     }
     public static final class layout {
-        public static final int shoppinglist=0x7f030000;
+        public static final int registeritem=0x7f030000;
+        public static final int shoppinglist=0x7f030001;
     }
     public static final class menu {
         public static final int listmenu=0x7f050000;
@@ -28,5 +35,7 @@ public final class R {
         public static final int hello=0x7f040000;
         public static final int v1_menu_1=0x7f040002;
         public static final int v1_menu_register_item=0x7f040003;
+        public static final int v1_tv_item=0x7f040004;
+        public static final int v1_tv_store=0x7f040005;
     }
 }
